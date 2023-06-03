522b350bcf756144d106828e3a2c9aa86e2a21ec
C110156238 <102135194+C110156238@users.noreply.github.com>
2023-06-03 10:51:08 +0800

    app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 print(os.getenv('CHANNEL_SECRET'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
